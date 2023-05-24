a7fc8366cfde2a41a06d48673869a5d217cbe181
JaviBot00 <javibot00@gmail.com>
2023-05-24 19:55:38 +0200

    Peticion cambiada a POST

--- app/src/main/java/com/politecnicomalaga/mymarketlist/controller/entities/ServerData.kt
+++ app/src/main/java/com/politecnicomalaga/mymarketlist/controller/entities/ServerData.kt
@@ -1,28 +1,94 @@
 package com.politecnicomalaga.mymarketlist.controller.entities
 
-import com.politecnicomalaga.mymarketlist.controller.SQLite.MarketListSQLite
 import android.app.Activity
+import android.widget.Toast
+import com.politecnicomalaga.mymarketlist.R
+import com.politecnicomalaga.mymarketlist.controller.SQLite.MarketListSQLite
 import com.politecnicomalaga.mymarketlist.controller.http.MyRequest
+import com.politecnicomalaga.mymarketlist.view.activity.LoginActivity
+import com.politecnicomalaga.mymarketlist.view.activity.RegisterActivity
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 class ServerData(private val fromActivity: Activity) {
     private val DELIMITER1 = ";"
     private val DELIMITER2 = ","
     private val SELECT_TYPES_TABLES = "selectTypesTables.php"
     private val SELECT_PRODUCTS_FROM = "selectProductsFrom.php"
+    private val SELECT_FROM_DATABASE = "selectFromDatabase.php"
+    private val CREATE_DATABASE = "createDatabase.php"
 
-    private val mySQLite = MarketListSQLite(fromActivity)
 
     fun getServerProductTables() {
-        mySQLite.setWritable()
-        MyRequest(fromActivity).phpQuery(SELECT_TYPES_TABLES) { response ->
+        val requestBody1 = MultipartBody.Builder().setType(MultipartBody.FORM)
+            .addFormDataPart("username", "HotGuy").build()
+        MyRequest(fromActivity).phpQuery(SELECT_TYPES_TABLES, requestBody1) { response ->
             val tables = response.split(DELIMITER1)
             checkTables(tables)
             tables.forEach { t ->
                 if (t.isNotEmpty()) {
                     val aux = t.split(DELIMITER2)
-                    MyRequest(fromActivity).phpQuery("$SELECT_PRODUCTS_FROM?table=" + aux[1]) { productsResponse ->
+                    val requestBody2 = MultipartBody.Builder().setType(MultipartBody.FORM)
+                        .addFormDataPart("table", aux[1]).build()
+                    MyRequest(fromActivity).phpQuery(
+                        SELECT_PRODUCTS_FROM, requestBody2
+                    ) { productsResponse ->
                         val products = productsResponse.split(DELIMITER1)
                         checkProducts(aux[1], products)
+                    }
+                }
+            }
+            println("kkk")
+        }
+    }
+
+    fun getClientUser(username: String, password: String) {
+        val requestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
+            .addFormDataPart("username", username).build()
+        MyRequest(fromActivity).phpQuery(
+            SELECT_FROM_DATABASE, requestBody
+        ) { response ->
+            val values: List<String> = response.split(DELIMITER2)
+            if (response.isNotEmpty() && (values[0] == username && values[1] == password)) {
+                LoginActivity.getInstance().doAccess(fromActivity)
+            } else {
+                Toast.makeText(
+                    fromActivity,
+                    fromActivity.resources.getString(R.string.user_not_exist),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    fun setClientUser(username: String, password: String, email: String, imgProfile: ByteArray) {
+        val requestBody1 = MultipartBody.Builder().setType(MultipartBody.FORM)
+            .addFormDataPart("username", username).build()
+        MyRequest(fromActivity).phpQuery(
+            SELECT_FROM_DATABASE, requestBody1
+        ) { response1 ->
+            val values: List<String> = response1.split(DELIMITER2)
+            if (response1.isNotEmpty() && (values[0] == username && values[1] == password)) {
+                Toast.makeText(
+                    fromActivity,
+                    fromActivity.resources.getString(R.string.user_exist),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                val requestBody2 = MultipartBody.Builder().setType(MultipartBody.FORM)
+                    .addFormDataPart("username", username).addFormDataPart("pass", password)
+                    .addFormDataPart("email", email).addFormDataPart(
+                        "imgProfile",
+                        "image.png",
+                        RequestBody.create("image/png".toMediaTypeOrNull(), imgProfile)
+                    ).build()
+                MyRequest(fromActivity).phpQuery(CREATE_DATABASE, requestBody2) { response2 ->
+                    val state = response2.split(DELIMITER2)
+                    if (state.all { it == "OK" }) {
+                        RegisterActivity.getInstance().endRegister(fromActivity)
+                    } else {
+                        TODO()
                     }
                 }
             }
@@ -30,6 +96,8 @@ class ServerData(private val fromActivity: Activity) {
     }
 
     private fun checkTables(tCloud: List<String>) {
+        val mySQLite = MarketListSQLite(fromActivity)
+        mySQLite.setWritable()
         tCloud.forEach { t ->
             if (t.isNotEmpty()) {
                 val aux = t.split(DELIMITER2)
@@ -37,14 +105,18 @@ class ServerData(private val fromActivity: Activity) {
                 mySQLite.createTable(aux[1])
             }
         }
+        mySQLite.getDb().close()
     }
 
     private fun checkProducts(table: String, pCloud: List<String>) {
+        val mySQLite = MarketListSQLite(fromActivity)
+        mySQLite.setWritable()
         pCloud.forEach { p ->
             if (p.isNotEmpty()) {
                 val aux = p.split(DELIMITER2)
                 mySQLite.insertProduct(table, aux[0], aux[1])
             }
         }
+        mySQLite.getDb().close()
     }
 }
--- app/src/main/java/com/politecnicomalaga/mymarketlist/controller/http/MyRequest.kt
+++ app/src/main/java/com/politecnicomalaga/mymarketlist/controller/http/MyRequest.kt
@@ -1,10 +1,13 @@
 package com.politecnicomalaga.mymarketlist.controller.http
 
 import android.app.Activity
+import android.os.Handler
+import android.os.Looper
 import com.google.android.material.snackbar.Snackbar
 import com.politecnicomalaga.mymarketlist.R
 import okhttp3.Call
 import okhttp3.Callback
+import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -19,25 +22,28 @@ class MyRequest(val fromActivity: Activity) {
 //    private val host: String = "http://79.147.83.111:8080/PhpSql/"
     private val client: OkHttpClient = OkHttpClient()
 
-    fun phpQuery(query: String, onResponseReceived: (String) -> Unit) {
+    fun phpQuery(query: String, requestBody: MultipartBody, onResponseReceived: (String) -> Unit) {
         val url = host + query
-        val request = Request.Builder()
-            .url(url)
-            .get()
-            .addHeader("cache-control", "no-cache")
-            .build()
+        val request =
+            Request.Builder().url(url).post(requestBody).addHeader("cache-control", "no-cache").build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                Snackbar.make(
-                    fromActivity.findViewById(android.R.id.content),
-                    fromActivity.getString(R.string.host_unreachable),
-                    Snackbar.LENGTH_LONG
-                ).show()
+                val myHandler = Handler(Looper.getMainLooper())
+                myHandler.post {
+                    Snackbar.make(
+                        fromActivity.findViewById(android.R.id.content),
+                        fromActivity.getString(R.string.host_unreachable),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
             }
 
             override fun onResponse(call: Call, response: Response) {
-                onResponseReceived(response.body.string())
+                val myHandler = Handler(Looper.getMainLooper())
+                myHandler.post {
+                    onResponseReceived(response.body.string())
+                }
             }
         })
     }
