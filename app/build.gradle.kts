4a480739422dac481fadbaeb595ae7985507bbd7
HotGuy <javibot00@gmail.com>
2024-05-26 14:34:06 +0200

    Añadir conexion con firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
+    // Android
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // Firebase
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -37,20 +42,26 @@ android {
 
 dependencies {
 
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+//    implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
     // My implementations
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
     implementation("com.makeramen:roundedimageview:2.3.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-//    implementation("com.jjoe64:graphview:4.2.2")
-//    implementation("com.squareup.picasso:picasso:2.71828")
-//    implementation("com.github.Redman1037:TSnackBar:V2.0.0")
-//    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.jjoe64:graphview:4.2.2")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    //    implementation 'com.github.Redman1037:TSnackBar:V2.0.0'
+    //    implementation 'com.google.code.gson:gson:2.8.9'
 
     // Android implementations
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
