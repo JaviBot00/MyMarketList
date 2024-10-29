860a0e44621028fa5d5656ba77e5c9107b69202c
JaviBot00 <javier.botella@metadata.es>
2024-10-29 11:10:54 +0100

    Añadido menu lateral

--- app/src/main/java/com/politecnicomalaga/mymarketlist/view/vActivities/ControlPanelActivity.kt
+++ app/src/main/java/com/politecnicomalaga/mymarketlist/view/vActivities/ControlPanelActivity.kt
@@ -2,16 +2,24 @@ package com.politecnicomalaga.mymarketlist.view.vActivities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.politecnicomalaga.mymarketlist.R
 import com.politecnicomalaga.mymarketlist.controller.MainController
 import com.politecnicomalaga.mymarketlist.controller.cEntities.ServerData
 import com.politecnicomalaga.mymarketlist.view.vFragments.Nav1ListsFragment
 import com.politecnicomalaga.mymarketlist.view.vFragments.Nav2DetailsFragment
 
-class ControlPanelActivity : AppCompatActivity() {
+
+class ControlPanelActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
 
     companion object {
         const val CATALOGUE_REQUEST = 1000
@@ -20,13 +28,28 @@ class ControlPanelActivity : AppCompatActivity() {
         const val SUGGEST_REQUEST = 4000
     }
 
+    private var mDrawerLayout: DrawerLayout? = null
+    private var mDrawerToggle: ActionBarDrawerToggle? = null
+    private val navigationView: NavigationView? = null
     private lateinit var bottomNav: BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control_panel)
         MainController().setControllers(this@ControlPanelActivity, R.string.app_name, "")
         ServerData(this@ControlPanelActivity).updateServerLists()
+
+        val toolbar: Toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        mDrawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        mDrawerToggle = ActionBarDrawerToggle(
+            this, mDrawerLayout, toolbar, R.string.app_name, R.string.in_maintenance
+        )
+        mDrawerLayout!!.setDrawerListener(mDrawerToggle)
+        mDrawerToggle!!.syncState()
+
 
         loadFragment(Nav1ListsFragment(this@ControlPanelActivity))
         bottomNav = findViewById(R.id.bottomNav)
@@ -89,5 +112,11 @@ class ControlPanelActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
