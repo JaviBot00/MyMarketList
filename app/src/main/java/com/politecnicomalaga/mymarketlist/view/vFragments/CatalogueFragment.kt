a54e2d2f50963b34cc2971759d84595a8d0feb25
JaviBot00 <javier.botella@metadata.es>
2024-10-30 13:23:23 +0100

    [Añadir-viewBinding] Hacer un binding en un fragment

--- app/src/main/java/com/politecnicomalaga/mymarketlist/view/vFragments/CatalogueFragment.kt
+++ app/src/main/java/com/politecnicomalaga/mymarketlist/view/vFragments/CatalogueFragment.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.politecnicomalaga.mymarketlist.R
 import com.politecnicomalaga.mymarketlist.controller.cAdapter.rvProducts.ProductsRVAdapter
+import com.politecnicomalaga.mymarketlist.databinding.FragmentRvCatalogueBinding
 import com.politecnicomalaga.mymarketlist.model.Product
 
 
@@ -17,6 +18,9 @@ class CatalogueFragment(
     private val fromActivity: Activity,
     private val myProducts: ArrayList<Product>
 ) : Fragment() {
+
+    private var _binding: FragmentRvCatalogueBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +30,15 @@ class CatalogueFragment(
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_rv_catalogue, container, false)
+        _binding = FragmentRvCatalogueBinding.inflate(inflater, container , false)
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_rv_catalogue, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val myRecyclerView: RecyclerView = view.findViewById(R.id.rvList)
+//        val myRecyclerView: RecyclerView = view.findViewById(R.id.rvList)
+        val myRecyclerView: RecyclerView = binding.rvList
         val myAdapter = ProductsRVAdapter(fromActivity, myProducts)
         myRecyclerView.layoutManager =
             LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
