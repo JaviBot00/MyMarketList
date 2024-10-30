e74f44eb20bbdb0b97925d23b45cc60ee5f4c498
HotGuy <javibot00@gmail.com>
2024-10-30 19:31:42 +0100

    [Implement-firebase] Actualizacion librerias & formatos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 android {
     namespace = "com.politecnicomalaga.mymarketlist"
     compileSdk = 34
+    buildToolsVersion = "35.0.0"
 
     defaultConfig {
         applicationId = "com.politecnicomalaga.mymarketlist"
@@ -32,18 +33,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
 dependencies {
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
 //    implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
@@ -58,13 +59,15 @@ dependencies {
     implementation("com.squareup.picasso:picasso:2.71828")
     //    implementation 'com.github.Redman1037:TSnackBar:V2.0.0'
     //    implementation 'com.google.code.gson:gson:2.8.9'
+//    implementation("com.android.tools.build:gradle:8.5.2")
 
     // Android implementations
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.1")
-        classpath("com.google.gms:google-services:4.4.2")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.1")
-    }
-}
+//buildscript {
+//    dependencies {
+//        classpath("com.android.tools.build:gradle:8.5.2")
+//        classpath("com.google.gms:google-services:4.4.2")
+//        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.2")
+//    }
+//}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // Android
+    id("com.android.application") version "8.7.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+
+    // Firebase
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
