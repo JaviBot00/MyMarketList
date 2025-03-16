b13c80945914c8dcc09deaa41f16851aae47b922
HotGuy <javibot00@gmail.com>
2025-03-16 15:07:00 +0100

    [Modificar-Vista-Principal] Update Versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 dependencies {
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
 //    implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
@@ -70,8 +70,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.9.3")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // Android
-    id("com.android.application") version "8.7.1" apply false
+    id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 
     // Firebase
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
